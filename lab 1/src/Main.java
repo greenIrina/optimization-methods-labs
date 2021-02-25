33ab7b7005a2c9535d6ff7cbf295051894035586
Irina <45588607+greenIrina@users.noreply.github.com>
2021-02-25 22:10:30 +0300

    add out

--- lab 1/src/Main.java
+++ lab 1/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // func y(x) = (log(10,(x-2)))^2 + (log(10,(10-x)))^2 - x^(0.2)
+        // func f(x) = (log(10,(x-2)))^2 + (log(10,(10-x)))^2 - x^(0.2)
         // range [6; 9.9]
-        // min y(x): x = 8.7269
+        // min f(x): x = 8.7269, f(x) = -0.8460
         double leftBound = 6, rightBound = 9.9;
-        Dichotomy dichotomy = new Dichotomy(leftBound, rightBound);
+        Solver dichotomy = new Dichotomy(leftBound, rightBound);
         double xDichotomy = dichotomy.getMinX(), funcDichotomy = dichotomy.getMinFunc();
         System.out.println("Точка минимумса: " + xDichotomy + ", минимум: " + funcDichotomy);
     }
