f8d776628cfbcfd3069be08423dbf653e082de91
Irina <45588607+greenIrina@users.noreply.github.com>
2021-02-28 20:46:53 +0300

    clean

--- lab 1/src/Fibonacci.java
+++ lab 1/src/Fibonacci.java
@@ -10,7 +10,12 @@ public class Fibonacci extends AbstractSolver implements Solver {
     }
 
     private double fibonacciNum(int k) {
-        return  Math.pow((1 + Math.sqrt(5)) / 2, k) / Math.sqrt(5);
+        return Math.pow((1 + Math.sqrt(5)) / 2, k) / Math.sqrt(5);
+        /* if (k <= 1) {
+            return 1;
+        } else {
+            return fibonacciNum(k - 1) + fibonacciNum(k - 2);
+        } */
     }
 
     private int countIterationsNum() {
@@ -28,7 +33,7 @@ public class Fibonacci extends AbstractSolver implements Solver {
     private void calcMinX(boolean printSteps) {
         double a = leftBound, b = rightBound;
         double x1 = a + fibonacciNum(iterationsNum) * (b - a) / fibonacciN2;
-        double x2 = a + fibonacciNum(iterationsNum + 1) * (b - a) / fibonacciN2;
+        double x2 = a + b - x1;
         double fX1 = calcFunc(x1);
         double fX2 = calcFunc(x2);
         int count = 0;
--- lab 1/src/Main.java
+++ lab 1/src/Main.java
@@ -3,9 +3,7 @@ public class Main {
         // func f(x) = (log(10,(x-2)))^2 + (log(10,(10-x)))^2 - x^(0.2)
         // range [6; 9.9]
         // min f(x): x = 8.7269, f(x) = -0.8460
-        //double leftBound = 6, rightBound = 9.9;
-        //double leftBound = -0.5, rightBound = 0.5;
-        double leftBound = -3, rightBound = 5;
+        double leftBound = 6, rightBound = 9.9;
         Solver dichotomy = new Dichotomy(leftBound, rightBound);
         double xDichotomy = dichotomy.getMinX(), funcDichotomy = dichotomy.getMinFunc();
         System.out.println("Метод дихотомии. Точка минимума: " + xDichotomy + ", минимум: " + funcDichotomy);
