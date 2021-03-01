a50f10e168260e8dd3c2f16b20ed9e07c6a25f9e
Irina <45588607+greenIrina@users.noreply.github.com>
2021-03-01 18:18:19 +0300

    create Parabola.java

---
+++ lab 1/src/Parabola.java
@@ -0,0 +1,16 @@
+public class Parabola extends AbstractSolver implements Solver {
+    @Override
+    public double getMinX() {
+        return 0;
+    }
+
+    @Override
+    public double getMinFunc() {
+        return 0;
+    }
+
+    @Override
+    public void printSteps() {
+
+    }
+}
